c7c0c16f658f303fbe726221cb1fe1e548c889d4
jnk0le <jnk0le@hotmail.com>
2017-12-25 23:18:31 +0100

    added unmasked implementation

--- README.md
+++ README.md
@@ -3,14 +3,61 @@
 - C++11 and above
 - no exceptions/RTTI
 - designed for compile time (static) allocation and type evaluation
-- lock-free atomic operation for SPSC cases
+- lock-free atomic operation in SPSC cases
 - underrun and overrun checks in insert/remove functions
-- currently implemented as array with 2 indices (n-1 available elements, `head == tail` empty state)
 - highly efficient on most microcontroller architectures (eg. avr, cortex-m)
+
+## available implementations
+
+
+### "Always Keep One Slot Open"
+
+- n-1 available elements (`head == tail` empty state)
+- implemented in `Ringbuffer` class
+
+### Counting total elements written/read (aka unmasked indices)
+
+- masking is performed when writing/reading array
+- insert operation might be a little slower due to comparison with (usually) large immediates.
+
+```
+// gcc 6.2 breaks it even more by doing weird unnecessary operations
+// on avr it gets even worser
+          unmasked_insert:
+08000268:   ldr     r2, [pc, #28]
+0800026a:   ldr     r3, [r2, #0]
+0800026c:   ldr     r1, [r2, #0] <--- ???
+0800026e:   ldr     r0, [r2, #4]
+08000270:   subs    r1, r1, r0 <-- ?? subs r1, r3, r0
+08000272:   cmp     r1, #8
+08000274:   beq.n   0x8000284
+08000276:   adds    r1, r3, #1
+08000278:   and.w   r3, r3, #7
+0800027c:   add     r3, r2
+0800027e:   movs    r0, #2
+08000280:   strb    r0, [r3, #8]
+08000282:   str     r1, [r2, #0]
+08000284:   bx      lr
+          masked_insert:
+0800028c:   ldr     r2, [pc, #24]
+0800028e:   ldr     r3, [r2, #0]
+08000290:   ldr     r1, [r2, #4]
+08000292:   adds    r3, #1
+08000294:   and.w   r3, r3, #7
+08000298:   cmp     r3, r1
+0800029a:   beq.n   0x80002a4
+0800029c:   adds    r1, r2, r3
+0800029e:   movs    r0, #1
+080002a0:   strb    r0, [r1, #8]
+080002a2:   str     r3, [r2, #0]
+080002a4:   bx      lr
+```
+
+- implemented in `Ringbuffer_unmasked` class
 
 ## notes
 
-- if ring buffer is allocated on the stack (local scope) or heap, it have to be explicitly cleared before use or be creadet using ... constructor
+- if ring buffer is allocated on the stack (local scope) or heap, it have to be explicitly cleared before use or be createt using value initializing constructor
 
 ```
 Ringbuffer<uint8_t, 256> a; // global objects can use empty constructor // it is zero initialized through bss section
@@ -25,7 +72,6 @@ int main()
 ```
 
 - index_t of size less than architecture reg size (size_t) might not be most efficient (arm gcc can still generate `uxth/uxtb` when not necessary)
-- 8 bit architectures other than AVR are not fixed yet
 
 ## example
 
@@ -62,6 +108,5 @@ extern "C" void USART2_IRQHandler(void)
 - block write/read
 - insert overwrite
 - pick appropriate namespace that will not end in "using namespace"
-- fix 8bit archs
-- more implementations
 - memory barriers for non gcc compilers
+- multi core ??
--- ringbuffer.hpp
+++ ringbuffer.hpp
@@ -125,7 +125,8 @@ template<typename T, size_t buffer_size = 16, typename index_t = uint_fast8_t>
 
 	private:
 		constexpr static index_t buffer_mask = buffer_size-1;
-		volatile index_t head, tail;
+		volatile index_t head;
+		volatile index_t tail;
 
 		T data_buff[buffer_size]; // put it at the bottom of the class to allow easier (object base pointer + offsets) access to other variables
 
@@ -138,11 +139,138 @@ template<typename T, size_t buffer_size = 16, typename index_t = uint_fast8_t>
 	#ifdef __AVR_ARCH__ // there is no STL available for AVR architecture
 		static_assert(buffer_size-1 <= UINT_FAST8_MAX, "buffers larger than 256 elements are not implemented atomic"); // cover most UB cases
 		static_assert(sizeof(index_t) == sizeof(uint8_t), "indexing type larger than unit8_t should still be atomic (<256 bytes), but it doesn't make sense");
-		// container type sign cannot be checked at the moment
-	#else // just do some regular assertions
+		// container type cannot be asserted at the moment
+		// container type sign cannot be asserted at the moment
+	#else
 		static_assert(std::numeric_limits<index_t>::is_integer, "indexing type is not integral type");
 		static_assert(!(std::numeric_limits<index_t>::is_signed), "indexing type shall not be signed");
-		static_assert(buffer_mask <= std::numeric_limits<index_t>::max(), "buffer size is too large for a given indexing type");
+		static_assert(buffer_mask <= std::numeric_limits<index_t>::max(), "buffer size is too large for a given indexing type (maximum size for n-bit variable is 2^n)");
+	#endif
+	};
+
+template<typename T, size_t buffer_size = 16, typename index_t = uint_fast8_t>
+	class Ringbuffer_unmasked
+	{
+	public:
+		Ringbuffer_unmasked() {} // empty - it have to be statically allocated during compilation
+		Ringbuffer_unmasked(int val)
+		{
+			head = val;
+			tail = val;
+		}
+
+		~Ringbuffer_unmasked() {} // empty - it have to be statically allocated during compilation
+
+		void clear(void)
+		{
+			head = tail;
+		}
+
+		bool isEmpty(void)
+		{
+			return readAvailable() == 0;
+		}
+
+		bool isFull(void)
+		{
+			return writeAvailable() == 0;
+		}
+
+		index_t readAvailable(void)
+		{
+			return head - tail;
+		}
+
+		index_t writeAvailable(void)
+		{
+			return buffer_size - (head - tail);
+		}
+
+		bool insert(T data)
+		{
+			index_t tmpHead = head; // reduce overhead of accessing volatile variables and make SPSC (interrupt<->thread) access atomic
+
+			if((head - tail) == buffer_size)
+				return false;
+			else
+			{
+				data_buff[tmpHead++ & buffer_mask] = data;
+
+				asm volatile("":::"memory");
+				head = tmpHead; // write it back after writing element - consumer now can read this element
+			}
+			return true;
+		}
+
+		bool insert(T* data)
+		{
+			index_t tmpHead = head; // reduce overhead of accessing volatile variables and make SPSC (interrupt<->thread) access atomic
+
+			if((head - tail) == buffer_size)
+				return false;
+			else
+			{
+				data_buff[tmpHead++ & buffer_mask] = *data;
+
+				asm volatile("":::"memory");
+				head = tmpHead; // write it back after writing element - consumer now can read this element
+			}
+			return true;
+		}
+
+		bool remove(T& data)
+		{
+			index_t tmpTail = tail; // reduce overhead of accessing volatile variables and make SPSC (interrupt<->thread) access atomic
+
+			if(tmpTail == head)
+				return false;
+			else
+			{
+				data = data_buff[tmpTail++ & buffer_mask];
+
+				asm volatile("":::"memory");
+				tail = tmpTail;  // write it back after reading element - producer can now use this location for new element
+			}
+			return true;
+		}
+
+		bool remove(T* data)
+		{
+			index_t tmpTail = tail; // reduce overhead of accessing volatile variables and make SPSC (interrupt<->thread) access atomic
+
+			if(tmpTail == head)
+				return false;
+			else
+			{
+				*data = data_buff[tmpTail++ & buffer_mask];
+
+				asm volatile("":::"memory");
+				tail = tmpTail;  // write it back after reading element - producer can now use this location for new element
+			}
+			return true;
+		}
+
+	private:
+		constexpr static index_t buffer_mask = buffer_size-1;
+		volatile index_t head;
+		volatile index_t tail;
+
+		T data_buff[buffer_size]; // put it at the bottom of the class to allow easier (object base pointer + offsets) access to other variables
+
+		// let's assert that no UB will be compiled
+		static_assert((buffer_size != 0), "buffer cannot be of zero size");
+		static_assert(!(buffer_size & buffer_mask), "buffer size is not a power of 2");
+		static_assert(sizeof(index_t) <= sizeof(size_t), "indexing type size is larger than size_t, operation is not implemented atomic and doesn't make sense");
+
+	#ifdef __AVR_ARCH__ // there is no STL available for AVR architecture
+		static_assert(((buffer_size-1) >> 1) <= UINT_FAST8_MAX, "buffers larger than 128 elements are not implemented atomic"); // cover most UB cases
+		static_assert(sizeof(index_t) == sizeof(uint8_t), "indexing type larger than unit8_t should still be atomic (<128 bytes), but it doesn't make sense");
+		// container type cannot be asserted at the moment
+		// container type sign cannot be asserted at the moment
+	#else
+		static_assert(std::numeric_limits<index_t>::is_integer, "indexing type is not integral type");
+		static_assert(!(std::numeric_limits<index_t>::is_signed), "indexing type shall not be signed");
+		static_assert(buffer_mask <= (std::numeric_limits<index_t>::max() >> 1), "buffer size is too large for a given indexing type (maximum size for n-bit variable is 2^(n-1))");
 	#endif
 	};
 
